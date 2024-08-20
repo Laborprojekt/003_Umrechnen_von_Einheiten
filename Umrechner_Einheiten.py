5126c65cc1b8e64a514655deeaf4e60833baf134
Laborprojekt <lejonina@tutanota.com>
2024-08-20 09:32:55 +0200

    planung

---
+++ Umrechner_Einheiten.py
@@ -0,0 +1,25 @@
+'''
+#-----------------------#
+Programm: 		Einheitenumrechner
+Version: 		V1.0
+
+Programmierer: 	Eberlei
+Datum:			20.08.2024
+'''
+
+def cm_inch
+
+def km_mile
+
+def liter_beer
+
+def kg_pound
+
+def celsius_kelvin
+
+print(
+    "\n------------\n"
+    "Chose unit you want do transfer:\n"
+    "1. cm to inch \n"
+    "2. km to eng. mile / international seemile \n"
+      
