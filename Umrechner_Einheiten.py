5e580aff83e3a03a4bd139ce7ffd68ccb75ba847
Laborprojekt <lejonina@tutanota.com>
2024-08-20 10:09:50 +0200

    Unit menü add

--- Umrechner_Einheiten.py
+++ Umrechner_Einheiten.py
@@ -5,21 +5,44 @@ Version: 		V1.0
 
 Programmierer: 	Eberlei
 Datum:			20.08.2024
+#-----------------------#
 '''
 
-def cm_inch
+'''
+def cm_inch():
 
-def km_mile
+def km_mile():
 
-def liter_beer
+def liter_beer():
 
-def kg_pound
+def kg_pound():
 
-def celsius_kelvin
+def celsius_kelvin():
+
+def float_check (num):
+     
+     try:
+         float_num = float(num)
+         return float_num
+        
+    except ValueError:
+        return False
+ '''   
+
 
 print(
-    "\n------------\n"
-    "Chose unit you want do transfer:\n"
+    "\nChoose unit you want do transfer:\n"
+
+    "------------\n" #12x -
     "1. cm to inch \n"
     "2. km to eng. mile / international seemile \n"
-      
+    "3. liter to beer \n"
+    "4. kg to pound \n"
+    "5. Celsius to Kelvin \n"
+    "------------"
+    )
+Unit_num = input("Unit number: ")
+
+Input = input("\nInput: ")
+              
+              
